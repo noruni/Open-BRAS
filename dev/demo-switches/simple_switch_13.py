6fdcec7f549357f0f9a40f17117363ed6f2ff943
Craig Osborne <cyclades.nz@gmail.com>
2014-04-09 12:20:37 +1200

    Simple formatting

--- dev/demo-switches/simple_switch_13.py
+++ dev/demo-switches/simple_switch_13.py
@@ -75,7 +75,7 @@ class SimpleSwitch13(app_manager.RyuApp):
         dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
 
-        self.logger.info("packet in dpid:%s src:%s dst:%s in_port%s", dpid, src, dst, in_port)
+        self.logger.info("packet in dpid:'%s' src:'%s' dst:'%s' in_port:'%s'", dpid, src, dst, in_port)
 
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
